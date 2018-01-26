39b05a1366128d77826cfb947f7bb22432d91288
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-01-26 13:06:29 -0700

    Made yasuoFilter method and added code for the filter in picture class.

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -173,6 +173,13 @@ public class Picture extends SimplePicture
       }
     }   
   }
+  
+  public void yasuoFilter(int startRow, int startCol)
+  {
+	  Pixel fromPixel = null;
+	  Pixel toPixel null;
+	  Picture yasuo = new Picture("yasuo.png");
+  }
 
   /** Method to create a collage of several pictures */
   public void createCollage()
